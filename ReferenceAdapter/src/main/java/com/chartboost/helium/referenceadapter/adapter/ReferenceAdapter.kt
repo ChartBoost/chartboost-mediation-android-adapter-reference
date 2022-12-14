9fab021814b7fff4b8a3667726061e35fb607f46
Vu <vu.chau@chartboost.com>
2022-12-14 17:55:42 -0500

    Adapter Audits, Dec 14 (#18)

--- ReferenceAdapter/src/main/java/com/chartboost/helium/referenceadapter/adapter/ReferenceAdapter.kt
+++ ReferenceAdapter/src/main/java/com/chartboost/helium/referenceadapter/adapter/ReferenceAdapter.kt
@@ -39,12 +39,15 @@ class ReferenceAdapter : PartnerAdapter {
 
     /**
      * Override this value to return the version of the mediation adapter.
-     * To determine the version, use the following scheme to indicate compatibility:
      *
-     * [Helium SDK Major Version].[Partner SDK Major Version].[Partner SDK Minor Version].[Partner SDK Patch Version].[Adapter Version]
+     * You may version the adapter using any preferred convention, but it is recommended to apply the
+     * following format if the adapter will be published by Helium:
      *
-     * For example, if this adapter is compatible with Helium SDK 2.x.y and partner SDK 1.0.0, and
-     * this is its initial release, then its version should be 2.1.0.0.0.
+     * Helium.Partner.Adapter
+     *
+     * "Helium" represents the Helium SDK’s major version that is compatible with this adapter. This must be 1 digit.
+     * "Partner" represents the partner SDK’s major.minor.patch.x (where x is optional) version that is compatible with this adapter. This can be 3-4 digits.
+     * "Adapter" represents this adapter’s version (starting with 0), which resets to 0 when the partner SDK’s version changes. This must be 1 digit.
      */
     override val adapterVersion: String
         get() = BuildConfig.HELIUM_REFERENCE_ADAPTER_VERSION
